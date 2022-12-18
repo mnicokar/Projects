4cbb93918aee9750269156450d6e7eb277225dda
mnicokar <mnicokar@ucsd.edu>
2022-12-18 13:02:26 -0800

    addReactStuff

---
+++ Module_3/React/BuildReactInfoSite/FirstProject/index.js
@@ -0,0 +1,15 @@
+
+//What do I want to render, where do i want to render it
+
+/*
+ReactDOM.render(<h1>Hello, Everyone</h1>, document.getElementById("root"));
+*/
+
+ReactDOM.render(<ul><li>Thing 1</li><li>Thing 2</li></ul>,document.getElementById("root"));
+
+//Why we learn react?
+/*
+Composable. Little parts come together to make big things
+Code becomes more manageable. 
+
+*/
